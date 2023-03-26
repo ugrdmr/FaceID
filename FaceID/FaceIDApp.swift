39fe3eb1e7cac2737db757c0ae10d23bba3935d1
Ugur Demir <ugrdmr@yahoo.com>
2023-03-26 14:15:28 -0400

    First Commit

---
+++ FaceID/FaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDApp.swift
+//  FaceID
+//
+//  Created by Ugur Demir on 2023-03-26.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
